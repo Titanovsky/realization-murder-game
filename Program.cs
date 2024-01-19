9f9a7a5080b6d2bb7845507b5129248dfe6201b0
Titanovsky <64440118+Titanovsky@users.noreply.github.com>
2024-01-19 21:24:34 +0500

    add checks and small changes

--- Program.cs
+++ Program.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
+using System.Linq.Expressions;
 using System.Numerics;
 using System.Runtime.CompilerServices;
+using System.Linq;
 
 namespace TheBestCourse;
-static class Program {
-	private static float _chanceForPlayerWithSecretGun = .43f;
-	private static float _chanceForPlayrIsDetective = .25f;
+static class Program { 
+	private const float _chanceForPlayerWithSecretGun = .43f;
+	private const float _chanceForPlayerIsDetective = .25f;
 	private static string[] _players = {"titanovsky", "zozda", "ExtraDip", "0xacgh", "LOLIK403"};
 	private static Queue<string> _murders = new();
 	private static string _detective = string.Empty;
@@ -40,18 +42,25 @@ static class Program {
 		ChooseMurder();
 		ChooseDetective();
 		ChoosePlayerWithGun();
+
+		End();
 	}
 
 	private static void End()
 	{
-		
+		Console.WriteLine("The game is end");
+	}
+
+	private static bool IsPlayerOnline(string ply)
+	{
+		return _players.Contains(ply);
 	}
 
 	private static void RefreshMurders()
 	{
 		int randomPlayerID = _rand.Next(0, _players.Length);
 		string playerToMurdersQueue = _players[randomPlayerID];
-        if (_murders.Count > 0 && playerToMurdersQueue.Equals(_murders.Last())) { Console.WriteLine("AAA"); RefreshMurders(); return; }
+        if (_murders.Count > 0 && playerToMurdersQueue.Equals(_murders.Last())) { RefreshMurders(); return; } // for a more variable murders
 
 		_murders.Enqueue(playerToMurdersQueue);
 	}
@@ -65,7 +74,7 @@ static class Program {
 			if (_murder == ply) continue;
 			if (_playerWithSecretGun == ply) continue;
 
-			bool has = MathF.Round(_rand.NextSingle(), 2) < _chanceForPlayrIsDetective;
+			bool has = MathF.Round(_rand.NextSingle(), 2) < _chanceForPlayerIsDetective;
 			if (!has) continue;
 
 			Console.WriteLine($"The player {ply} become the detective!");
@@ -83,7 +92,16 @@ static class Program {
 		if (_murders.Count == 0) { Error("The game can't choose a murder"); return; }
 
 		var murder = _murders.Dequeue();
-        //todo check on online Player
+		if (!IsPlayerOnline(murder))
+		{
+			for (int i = 0; i < _murders.Count; i++)
+			{
+				murder = _murders.Dequeue();
+				if (IsPlayerOnline(murder)) break;
+
+				ChooseMurder(); // show error
+			}
+		}
 
         Console.WriteLine($"The player {murder} will become the murder");
 
